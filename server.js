8d39667b40316a640033b43ba92e17ecb3c0f6dd
thesoftlifeintegrationstudio-byte <thesoftlifeintegrationstudio@gmail.com>
2026-02-02 16:50:58 +0530

    Initialize Express server with static file serving

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the current directory
+app.use(express.static(__dirname));
+
+// Route for the main page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Route for all other requests - serve the main page
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`PAUSE app is running on port ${PORT}`);
+    console.log(`Open http://localhost:${PORT} in your browser`);
+});
